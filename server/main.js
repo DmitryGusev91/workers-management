aa13958b053d40cbf129d2d765136f2e6ea133ed
DmitryGusev91 <Dmitrygusev91@gmail.com>
2024-02-14 23:05:58 +0200

    first try

--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./configs/db");
+const mongoose = require("mongoose");
+const path = require("path");
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
 const departmentsRouter = require("./routers/departmentRouter");
 const employeesRouter = require("./routers/employeeRrouter");
@@ -12,10 +15,10 @@ const authRouter = require("./routers/authRouter");
 const app = express();
 const port = 8000;
 
-connectDB();
-
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../client/")));
 
 // routers
 app.use("/auth", authRouter);
@@ -23,6 +26,10 @@ app.use("/departments", departmentsRouter);
 app.use("/employees", employeesRouter);
 app.use("/shifts", shiftsRouter);
 app.use("/users", usersRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/login.html"));
+});
 
 app.listen(port, () =>
   console.log(`app is listening at http://localhost:${port}`)
