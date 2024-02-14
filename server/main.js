ec12492d33b75e5fadef88b38f14f476b4e5fbef
DmitryGusev91 <Dmitrygusev91@gmail.com>
2024-02-14 23:28:46 +0200

    added env to main

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
+require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 
